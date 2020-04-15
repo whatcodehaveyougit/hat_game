f331c6c842326e84829849e2c7b7fe91a68c2868
whatcodehaveyougit <siggywatt@yahoo.com>
2020-04-15 11:33:51 +0100

    front end components being setup

---
+++ frontend/src/components/CreateGame.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function CreateGame(){
+
+
+    return (
+        <>
+    <button class="game">Create a New Game!</button>
+    </> 
+    )
+    
+}
--- frontend/src/containers/Dashboard.js
+++ frontend/src/containers/Dashboard.js
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from 'react'
+import CreateGame from '../components/CreateGame.js'
 
 function Dashboard() {
     
+    // Games is the state item
+    // setGames is the function that alters the state
+    // useState([]) tells it to expect an array 
     const [games, setGames] = useState([])
 
+    // This is basically ComponentDidMount due to the empty array, only gets rendered once
     useEffect(() => {
         fetch("/games/")
         .then(res => res.json())
@@ -17,6 +22,10 @@ function Dashboard() {
             <>
                 <h1>The Hat Game</h1>
                 <h3>These are the games today</h3>
+                { games.map ( game => (
+                    <button class="game">{game.title}</button>
+                ))}
+                <CreateGame />
                
             </>
         )
