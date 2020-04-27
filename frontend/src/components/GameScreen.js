c42bf28076c000c51624ab920c8c0f9cfaa9659c
whatcodehaveyougit <siggywatt@yahoo.com>
2020-04-27 16:50:27 +0100

    Before team score refactor

--- frontend/src/components/GameScreen.js
+++ frontend/src/components/GameScreen.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function GameScreen(props) {
+    const [game, setGame] = useState(props.selectedGame)
+    console.log(game);
+    
+
     return (
         <>
-        <p>Hello </p>
+        <p>Sigurd's turn to go next</p>
+        <button>Grab the Hat</button>
+
+        <h3>Dynamic Scores</h3>
+        <div>Team1: 10</div>
+        <div>Team2: 10</div>
+
         </>
     )
 }
