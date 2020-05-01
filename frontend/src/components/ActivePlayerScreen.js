019126392dc314ede4fac5a626f4ae365fcf5d3a
whatcodehaveyougit <siggywatt@yahoo.com>
2020-05-01 13:16:30 +0100

    clock now working after refactor

--- frontend/src/components/ActivePlayerScreen.js
+++ frontend/src/components/ActivePlayerScreen.js
@@ -4,25 +4,18 @@ import '../App.css'
 
 const ActivePlayerScreen = (props) => {
     
-    const [timerStarted, setTimerStarted] = useState(props.timerStarted)
-    const [seconds, setSeconds] = useState(60);
-    const [redirect, setRedirect] = useState(props.redirect)
+    // Timer
+    const [timerStarted, setTimerStarted] = useState(false)
+    const [clock, setClock] = useState(null)
+    const [timeLeft, setTimeLeft] = useState(5);
+    // Clues
     const [cluesArray, setCluesArray] = useState(props.selectedGame.clues)
     const [currentClue, setCurrentClue] = useState()
     const [counter, setCounter] = useState(0)
     const [emptyHatRedirect, setEmptyHatRedirect] = useState(false)
-
-    if (emptyHatRedirect){
-        return <Redirect to='/the-hat-is-empty'/>
-        }
-
-    if(redirect) {
-        return <Redirect to='/the-hat-game/turn-over' />
-    }    
     
     function startRound() {
-        setTimerStarted(false)
-        startTimer(1)
+        setTimerStarted(true)
         nextClue()
     }
 
@@ -40,33 +33,48 @@ const ActivePlayerScreen = (props) => {
     function isHatEmpty(){
         return new Promise((resolve, reject) => {     
         if (cluesArray.length === counter){
-            setEmptyHatRedirect(true) 
-            return reject      
+            setEmptyHatRedirect(true)
+            return reject    
         } else {
             setCurrentClue(cluesArray[counter].content)
         }
      })
     }
   
-    function startTimer(seconds) {
-        let clock = setInterval(function () {
-            seconds = seconds - 1    
-            setSeconds(seconds);
-        
-            if (seconds === 0) {
-                clearTimeout(clock)
-                return setRedirect(true)
+    useEffect(() => {
+        if (!timerStarted) return
+        console.log("useEffect ran");
+
+        const intervalId = setInterval(() => {
+          console.log("timer ticks");
+          setTimeLeft(prevTimeLeft => {
+            if (prevTimeLeft === 1) {
+              clearInterval(intervalId)
+              return <Redirect to='/the-hat-game/turn-over'/>
+            } else {
+              return prevTimeLeft - 1
             }
+          })
         }, 1000);
-    }
+
+        // clear interval on re-render to avoid memory leaks
+        return () => {
+            console.log("interval was cleared");  
+            clearInterval(intervalId);
+          }
+        }, [timerStarted])
+
+       
 
     return (
         <>
-            { timerStarted ? <h1>Have you picked up the hat?</h1> : null }
-            { timerStarted ? <button className="start-clock" onClick={startRound}>Start the Clock</button> : null }
-            { timerStarted ? null : <div className="time-left"> {seconds} </div> }
-            { timerStarted ? null : <div>Current Clues: <br/><span className="current-clue">{currentClue}</span></div> }
-            { timerStarted ? null : <button onClick={nextClue}>Next Clue</button> }
+            { timerStarted ? null : <h1>Have you picked up the hat?</h1> }
+            { timerStarted ? null : <button className="start-clock" onClick={startRound}>Start the Clock</button> }
+            { timerStarted ? <div className="time-left"> {timeLeft} </div> : null}
+            { timerStarted ? <div>Current Clues: <br/><span className="current-clue">{currentClue}</span></div> : null }
+            { timerStarted ? <button onClick={nextClue}>Next Clue</button> : null }  
+            { emptyHatRedirect ? <Redirect to='/the-hat-is-empty'/> : null }       
+        }
         </>
     )
 }
